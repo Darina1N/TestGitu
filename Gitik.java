c7726eb900e75766a4f6172846482a33b4a71cb2
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:32:28 +0100

    Update Gitik.java
    changed

--- Gitik.java
+++ Gitik.java
@@ -3,11 +3,16 @@ package sk.kosickaakademia.kolesarova;
 public class Gitik {
     private int number;
 
+    public void hi(){
+        
+    }
+    
     public void Number(){
-        for(int i=1;i>10; i++){
+        //comment
+        for(int i=10;i>100; i++){
             number+=i;
             System.out.println(number);
-            System.out.println("Dopĺňam aby bolo do čoho robiť zmeny.");
+            System.out.println("Dopĺňam aby bolo do čoho robiť zmeny. OK, precital som!");
         }
     }
 
