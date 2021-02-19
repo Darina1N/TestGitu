fc246b6a18c6a1b2c73acdcc1d2a6c13fa4d7dcf
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:31:33 +0100

    Update Gitik.java
    changed

--- Gitik.java
+++ Gitik.java
@@ -4,10 +4,15 @@ public class Gitik {
     private int number;
 
     public void Number(){
-        for(int i=1;i>10; i++){
+        for(int i=-5;i>10; i++){
             number+=i;
-            System.out.println(number);
+            System.out.println("Result: "+number);
         }
+    }
+    
+    // this is a comment
+    public void hello(){
+        // hello   
     }
 
 }
