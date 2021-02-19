b937168f9176326e3065d564cff81004b5d5c52b
Darina Kolesarova <darina.kolesarova@kosickaakademia.sk>
2021-02-19 08:33:07 +0100

    my first commit

---
+++ Gitik.java
@@ -0,0 +1,13 @@
+package sk.kosickaakademia.kolesarova;
+
+public class Gitik {
+    private int number;
+
+    public void Number(){
+        for(int i=1;i>10; i++){
+            number+=i;
+            System.out.println(number);
+        }
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+package sk.kosickaakademia.kolesarova;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Skúška gitu");
+        System.out.println("Keď to nepôjde tak ma trafí šľak");
+    }
+}
